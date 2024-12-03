39156d207599ad3b7d7eb0d20c7421a92242d658
Brian Perrett <perrettbrian@gmail.com>
2024-12-02 22:31:00 -0800

    rust format

--- rust/src/bin/day01.rs
+++ rust/src/bin/day01.rs
@@ -6,7 +6,7 @@ fn main() {
     let mut l2: Vec<i32> = vec![];
     let mut l2_counter = HashMap::<i32, i32>::new();
     for line in data.lines() {
-        let mut parts = line.split_whitespace().map(|x| {x.parse::<i32>().unwrap()});
+        let mut parts = line.split_whitespace().map(|x| x.parse::<i32>().unwrap());
         let a = parts.next().unwrap();
         let b = parts.next().unwrap();
         l1.push(a);
@@ -15,17 +15,18 @@ fn main() {
     }
     l1.sort_unstable();
     l2.sort_unstable();
-    let p1 = zip(&l1, &l2).map(|(a, b)| {
-        (a - b).abs()
-    }).sum::<i32>();
-    let p2 = l1.into_iter().map(|a| {
-        let b = if let Some(b) = l2_counter.get(&a) {
-            b
-        } else {
-            &0
-        };
-        a * b
-    }).sum::<i32>();
+    let p1 = zip(&l1, &l2).map(|(a, b)| (a - b).abs()).sum::<i32>();
+    let p2 = l1
+        .into_iter()
+        .map(|a| {
+            let b = if let Some(b) = l2_counter.get(&a) {
+                b
+            } else {
+                &0
+            };
+            a * b
+        })
+        .sum::<i32>();
     println!("Part 1: {}", p1);
     println!("Part 2: {}", p2);
 }
