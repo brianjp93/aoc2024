0a1376b3dbb41d16b78e99062b72e689468c7d73
Brian Perrett <bperrett@fusionbox.com>
2024-12-02 17:53:43 -0800

    Very short

--- python/day02.py
+++ python/day02.py
@@ -1,26 +1,10 @@
 from pathlib import Path
 
 data = (Path(__file__).parent.parent / 'data/day02.txt').read_text()
+parsed = list(map(lambda row: tuple(map(int, row.split())), data.splitlines()))
 
 def is_safe(level):
-    for a, b in zip(level, level[1:]):
-        if (b > a) != (level[1] > level[0]) or not (1 <= abs(a-b) < 4) or a == b:
-            return 0
-    return 1
+    return 0 if any((b > a) != (level[1] > level[0]) or not (0 < abs(a-b) < 4) for a, b in zip(level, level[1:])) else 1
 
-def p1():
-    return sum(is_safe(tuple(map(int, row.split()))) for row in data.splitlines())
-
-def p2():
-    safe_count = 0
-    for row in data.splitlines():
-        row = list(map(int, row.split()))
-        for i in range(len(row) + 1):
-            if is_safe(row[:i] + row[i+1:]):
-                safe_count += 1
-                break
-    return safe_count
-
-
-print(p1())
-print(p2())
+print(sum(is_safe(row) for row in parsed))
+print(sum(any(is_safe(row[:i] + row[i+1:]) for i in range(len(row))) for row in parsed))
