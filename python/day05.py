a6bc687ebf6e43ac05cc214a0a9963f4bb734f16
Brian Perrett <bperrett@fusionbox.com>
2024-12-05 01:14:17 -0800

    shorter

--- python/day05.py
+++ python/day05.py
@@ -1,27 +1,20 @@
 from pathlib import Path
 from functools import cmp_to_key
+from collections import defaultdict
 
 data = (Path(__file__).parent.parent / "data/day05.txt").read_text()
 
 
+def is_valid_page(page):
+    return not any(rules[n] & set(page[i+1:]) for i, n in enumerate(page))
+
 p1, p2 = data.split("\n\n")
-rules = {}
+rules = defaultdict(set)
 for line in p1.splitlines():
     a, b = map(int, line.split("|"))
-    if b not in rules:
-        rules[b] = set()
     rules[b].add(a)
 
 pages = [list(map(int, x.split(","))) for x in p2.splitlines()]
-
-
-def is_valid_page(page):
-    for i, n in enumerate(page):
-        if rule := rules.get(n, None):
-            if rule & set(page[i + 1 :]):
-                return False
-    return True
-
 
 p1 = p2 = 0
 for page in pages:
@@ -29,7 +22,7 @@ for page in pages:
         idx = len(page) // 2
         p1 += page[idx]
     else:
-        page = sorted(page, key=cmp_to_key(lambda x, y: -1 if y in rules[x] else 1))
+        page.sort(key=cmp_to_key(lambda x, y: -1 if y in rules[x] else 1))
         idx = len(page) // 2
         p2 += page[idx]
 
