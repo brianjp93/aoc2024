d10712715d83d529c2113c87753213f15e5a0357
Brian Perrett <bperrett@fusionbox.com>
2024-12-05 00:50:33 -0800

    better

--- python/day05.py
+++ python/day05.py
@@ -1,43 +1,27 @@
 from pathlib import Path
+from functools import cmp_to_key
 
-data = (Path(__file__).parent.parent / 'data/day05.txt').read_text()
+data = (Path(__file__).parent.parent / "data/day05.txt").read_text()
 
 
-p1, p2  = data.split("\n\n")
+p1, p2 = data.split("\n\n")
 rules = {}
 for line in p1.splitlines():
     a, b = map(int, line.split("|"))
-    if a not in rules:
-        rules[a] = {"before": set(), "after": set()}
     if b not in rules:
-        rules[b] = {"before": set(), "after": set()}
-    rules[a]["after"].add(b)
-    rules[b]["before"].add(a)
+        rules[b] = set()
+    rules[b].add(a)
 
 pages = [list(map(int, x.split(","))) for x in p2.splitlines()]
+
 
 def is_valid_page(page):
     for i, n in enumerate(page):
         if rule := rules.get(n, None):
-            before = set(page[:i])
-            after = set(page[i+1:])
-            if (rule["before"] & after) | (rule["after"] & before):
+            if rule & set(page[i + 1 :]):
                 return False
     return True
 
-def fix_page(page):
-    page = page[:]
-    while not is_valid_page(page):
-        for i, n in enumerate(page):
-            if rule := rules.get(n, None):
-                before = set(page[:i])
-                after = set(page[i+1:])
-                wrong = (rule["before"] & after) | (rule["after"] & before)
-                if wrong:
-                    n2 = wrong.pop()
-                    i2 = page.index(n2)
-                    page[i2], page[i] = page[i], page[i2]
-    return page
 
 p1 = p2 = 0
 for page in pages:
@@ -45,8 +29,9 @@ for page in pages:
         idx = len(page) // 2
         p1 += page[idx]
     else:
-        page = fix_page(page)
+        page = sorted(page, key=cmp_to_key(lambda x, y: -1 if y in rules[x] else 1))
         idx = len(page) // 2
         p2 += page[idx]
+
 print(p1)
 print(p2)
