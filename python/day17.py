2da0e8e9feb8f844bf18fcc5c109f348cc56d2dc
Brian Perrett <perrettbrian@gmail.com>
2024-12-17 19:49:30 -0800

    day 17

--- python/day17.py
+++ python/day17.py
@@ -1,25 +1,8 @@
 from pathlib import Path
+import heapq
 
 
 data = (Path(__file__).parent.parent / "data/day17.txt").read_text().strip()
-
-# data = """Register A: 729
-# Register B: 0
-# Register C: 0
-#
-# Program: 0,1,5,4,3,0"""
-
-# data = """Register A: 10
-# Register B: 0
-# Register C: 0
-#
-# Program: 5,0,5,1,5,4"""
-
-# data = """Register A: 2024
-# Register B: 0
-# Register C: 0
-#
-# Program: 0,3,5,4,3,0"""
 
 
 class HaltMachine(Exception):
@@ -47,12 +30,10 @@ class Machine:
             return n
         elif 4 <= n <= 6:
             idx = n - 4
-            # print(f"Getting combo at index {idx}")
             return self.registers[idx]
         assert False
 
     def dv(self, register):
-        # print(f"running division on register {register}")
         numerator = self.registers[0]
         den = 2 ** self.combo()
         self.registers[register] = numerator // den
@@ -67,45 +48,35 @@ class Machine:
         return self.get(self.idx + 1)
 
     def do_instruction(self):
-        # print(f"looking at {self.idx=}, {self.get(self.idx)=}")
         match self.get(self.idx):
             case 0:
-                # print('running 0')
                 self.dv(0)
             case 1:
-                # print('running 1')
                 ret = self.literal() ^ self.registers[1]
                 self.registers[1] = ret
                 self.idx += 2
             case 2:
-                # print('running 2')
                 self.registers[1] = self.combo() % 8
                 self.idx += 2
             case 3:
-                # print('running 3')
                 a = self.registers[0]
                 if a == 0:
                     self.idx += 2
                 else:
                     self.idx = self.literal()
             case 4:
-                # print('running 4')
                 b = self.registers[1]
                 c = self.registers[2]
                 self.registers[1] = b ^ c
                 self.idx += 2
             case 5:
-                # print('running 5')
                 ret = self.combo() % 8
-                # print(f'Got mod 8 value {ret}')
                 self.output.append(ret)
                 self.idx += 2
                 return self.output
             case 6:
-                # print('running 6')
                 self.dv(1)
             case 7:
-                # print('running 7')
                 self.dv(2)
 
     def reset(self):
@@ -119,62 +90,41 @@ class Machine:
             return False
         return self.program[:len(output)] == output
 
-    def find_dumb(self):
-        initial = 3889057078656
+    def run_at(self, a_register):
+        self.reset()
+        self.registers[0] = a_register
+        return self.run()
+
+    def binary_middle(self, low, high):
+        return low + ((high - low) // 2) - 1
+
+    def find_first_n_digit(self, n):
+        initial = 1
         while True:
-            self.reset()
-            self.registers[0] = initial
-            self.run()
-            print(initial)
-            print(self.output)
-            if self.output == self.program:
-                return initial
-            initial += 100000
-
-    def find(self):
-        initial = 2799023321088
-        found_high = False
-        high = float('inf')
-        low = 0
-        program = int(''.join(map(str, reversed(self.program))))
+            self.run_at(initial)
+            initial *= 2
+            if len(self.output) >= n:
+                break
+        high = initial
+        low = high // 2
+        initial = self.binary_middle(low, high)
         while True:
-            print(f"checking A={initial}")
-            self.reset()
-            self.registers[0] = initial
-            while True:
-                try:
-                    self.do_instruction()
-                except HaltMachine:
-                    break
-
-            print(f"{initial=}")
-            print(self.output)
-
-            output = int(''.join(map(str, reversed(self.output))))
-            if found_high:
-                print(f" {output=}")
-                print(f"{initial=}")
-                print(f"{low=}, {high=}")
-                if output < program:
-                    print('too high')
-                    high = initial
-                    initial = low + ((high - low) // 2)
-                elif output > program:
-                    print('too low')
-                    low = initial
-                    initial = low + ((high - low) // 2)
-                else:
+            if high == low:
+                return high
+            self.run_at(initial)
+            if len(self.output) == n:
+                self.run_at(initial - 1)
+                # if one previous gives us a shorter length, we found the correct initial value
+                if len(self.output) == n - 1:
                     return initial
-                input()
-            else:
-                if output > program:
-                    input()
-                    found_high = True
-                    high = initial
-                    low = high // 2
-                    initial = low + ((high - low) // 2)
                 else:
-                    initial = initial * 2
+                    # otherwise, we're still too high
+                    high = initial
+                    initial = self.binary_middle(low, high)
+            else:
+                # we're too low
+                low = initial
+                initial = self.binary_middle(low, high)
 
     def run(self):
         while True:
@@ -187,9 +137,48 @@ class Machine:
 machine = Machine(data)
 print(machine.run())
 machine.reset()
-# print(machine.program, machine.registers)
 # output = machine.run()
-# print(','.join(map(str,output)))
 
-# print(machine.find())
-print(machine.find_dumb())
+
+
+print(machine.find_first_n_digit(len(machine.program) - 1))
+print(machine.output)
+#
+# low = 35184372088832
+# high = 281474976710655
+
+indexes = [1] + [machine.find_first_n_digit(i) for i in range(2, len(machine.program) + 1)]
+
+# machine.run_at(low + 4398046511104)
+# machine.run_at(low + 4398046511103)
+#     indexes[-1]*7
+#     + indexes[-2]*2
+#     + indexes[-3]*2
+#     + indexes[-4]*6
+#     + indexes[-5]*4
+#     + indexes[-6]*2
+#     + indexes[-7]*4
+#     + indexes[-8]*5
+#     + indexes[-9]*5
+# ))
+
+
+def compute(coeffs):
+    return sum(c * indexes[-(i + 1)] for i, c in enumerate(coeffs))
+
+def search(m: Machine):
+    stack = [[x] for x in range(8)]
+    while stack:
+        coeffs = heapq.heappop(stack)
+        a = compute(coeffs)
+        idx = len(coeffs)
+        m.run_at(a)
+        if m.output == m.program:
+            return a
+        if m.program[-idx] == m.output[-idx]:
+            for i in range(8):
+                heapq.heappush(stack, coeffs + [i])
+
+
+p2 = search(machine)
+print(f"{p2=}")
