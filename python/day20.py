d0bba19f5cb280114ffc7d800544c369a7b81933
Brian Perrett <bperrett@fusionbox.com>
2024-12-24 08:39:58 -0800

    day 20

--- python/day20.py
+++ python/day20.py
@@ -1,9 +1,5 @@
 from pathlib import Path
 from aoc_utils import StringMap, DIRS_4, Point
-import heapq
-import sys
-
-sys.setrecursionlimit(10000000)
 
 
 data = (Path(__file__).parent.parent / "data/day20.txt").read_text().strip()
@@ -13,139 +9,54 @@ class Map(StringMap):
     pos_char = 'S'
     end_char = 'E'
 
-    def find_path(self, start, end):
+    def find_path(self, start, end, open=None):
         pos = start
-        pq = []
-        heapq.heappush(pq, (0, pos))
+        pq = [(0, pos)]
         seen = {}
         while pq:
-            items = heapq.heappop(pq)
+            items = pq.pop(0)
             score, pos = items
             if pos in seen:
                 continue
             seen[pos] = score
-            ch = self.m.get(pos, '#')
-            # if pos == self.end:
-            #     return score, seen
+
+            if open and pos in open:
+                ch = '.'
+            else:
+                ch = self.m.get(pos, '#')
+
+            if pos == end:
+                return score, seen
             if ch == '#':
                 continue
             for dir in DIRS_4:
-                heapq.heappush(pq, (score + 1, pos + dir))
+                pq.append((score + 1, pos + dir))
         return seen[end], seen
 
-    def build_dist_map(self, save_time=100):
-        old_min_dist, old_dmap = self.find_path(self.end, self.pos)
-        _, dmap_from_start = self.find_path(self.pos, self.end)
-        max_dist = old_min_dist - save_time
-        # print(f"{max_dist=}")
-        stack: list[tuple[int, Point, None | Point, int, set[Point]]] = [(self.pos.manhattan(self.end), self.pos, None, 0, set())]
-        # distance map
-        dmap = {}
-        while stack:
-            # print(f"{len(stack)=}")
-            item = heapq.heappop(stack)
-            best_possible, pos, cheat_pos, dist, cpath = item
-            key = (pos, cheat_pos)
-
-            if pos in cpath:
-                continue
-
-            if cheat_pos and key in dmap:
-                continue
-
-            if best_possible > max_dist:
-                # print("too big")
-                continue
-
-            ch = self.m.get(pos, '#')
-            if pos == self.end:
-                if key not in dmap:
-                    dmap[key] = {}
-                dmap[key][dist] = dmap[key].get(dist, 0) + 1
-                print(f"found a solution with distance {dist} which saves {old_min_dist - dist}")
-                continue
-            if ch == '#':
-                if cheat_pos:
-                    # print('already cheated')
+    def get_valid(self, pos: Point, man_dist: int):
+        for y in range(-man_dist, man_dist + 1):
+            x_range = man_dist - abs(y)
+            for x in range(-x_range, x_range + 1):
+                assert abs(x) + abs(y) <= man_dist
+                if x == 0 and y == 0:
                     continue
-                cheat_pos = pos
+                npos = pos + Point(x, y)
+                if self.m.get(npos, '#') == '.':
+                    yield npos
 
-            if dmap.get(key, None) is None:
-                dmap[key] = {dist: 1}
-            else:
-                if cheat_pos and dist > dmap_from_start.get(pos, 0):
-                    # print("impossible")
-                    continue
-                dmap[key][dist] = dmap[key].get(dist, 0) + 1
-            for dir in DIRS_4:
-                npath = cpath | {pos}
-                npos = pos + dir
-                ndist = dist + 1
-                if cheat_pos:
-                    best_possible = ndist + old_dmap.get(npos, float('inf'))
-                else:
-                    point_diff = npos.manhattan(self.end)
-                    best_possible = point_diff + ndist
-                heapq.heappush(stack, (best_possible, npos, cheat_pos, ndist, npath))
-        return dmap
-
-    def count_paths(self, pos: Point, dist: int, cheat_pos: Point | None, max_score, cache, cpath: list):
-        key = (pos, bool(cheat_pos))
-        if key in cache:
-            if dist <= cache[key]:
-                print(f"found item in cache {key}")
-                return 1
-
-        if pos in [x[0] for x in cpath]:
-            return 0
-        if dist > max_score:
-            return 0
-        if pos == self.end:
-            print('Found a possible solution with dist=', dist)
-            for pos, dist, cheat_pos in cpath:
-                key = (pos, bool(cheat_pos))
-                cache[key] = max(cache.get(key, 0), dist)
-            return 1
-
-        ch = self.m.get(pos, '#')
-
-        ncheat_pos = cheat_pos
-        if ch == '#':
-            if cheat_pos:
-                return 0
-            else:
-                ncheat_pos = pos
-
-        npath = cpath + [(pos, dist, cheat_pos)]
-        count = sum(self.count_paths(pos + dir, dist + 1, ncheat_pos, max_score, cache, npath) for dir in DIRS_4)
-        return count
+    def count_cheats(self, cheat=2, improvement: int=1):
+        track = {pos for pos, ch in self.m.items() if ch == '.'}
+        _, og_dmap = self.find_path(self.pos, self.end)
+        solutions = 0
+        for pos in track:
+            start_dist = og_dmap[pos]
+            for other in self.get_valid(pos, cheat):
+                end_dist = og_dmap[other]
+                if start_dist + pos.manhattan(other) <= end_dist - improvement:
+                    solutions += 1
+        return solutions
 
 
-# m = Map(data)
-# score, dmap = m.find_path(m.pos, m.end)
-# max_score = score - 100
-# print(m.count_paths(m.pos, 0, None, max_score, {}, []))
-
-
-
-
-data = """###############
-#...#...#.....#
-#.#.#.#.#.###.#
-#S#...#.#.#...#
-#######.#.#.###
-#######.#.#...#
-#######.#.###.#
-###..E#...#...#
-###.#######.###
-#...###...#...#
-#.#####.#.###.#
-#.#...#.#.#...#
-#.#.#.#.#.#.###
-#...#...#...###
-###############"""
 m = Map(data)
-score, dmap = m.find_path(m.pos, m.end)
-max_score = score - 1
-print(m.build_dist_map(save_time=1)[(m.end, )])
-# print(m.count_paths(m.pos, 0, None, max_score, {}, []))
+print(m.count_cheats(2, improvement=100))
+print(m.count_cheats(20, improvement=100))
